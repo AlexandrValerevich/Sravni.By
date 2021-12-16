38c358e3045141f825c75a78b3d0c0b8e725501d
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-12-17 00:37:42 +0300

    Finished configuring the project

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dest'),
+    filename: 'js/[name].[hash].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ['html-loader', 'pug-html-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.m?ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.pug',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    compress: false,
+    port: 9000,
+  },
+};
