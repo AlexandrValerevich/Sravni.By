2a0eb363ddbdaedff2d54a444c445b9e23681274
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-12-18 13:21:31 +0300

    Add search

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./style/style.scss";
 import "./index.pug";
 
 import "./ts/page-load/page-load";
+import "./ts/query-worker/query-worker";
--- src/ts/query-worker/query-worker.ts
+++ src/ts/query-worker/query-worker.ts
@@ -1,14 +1,19 @@
+import { getBooks } from '../get-books/get-books';
+import { CardManager } from '../card-manager/card-manager';
 
+let $search: HTMLInputElement = <HTMLInputElement>document.getElementById('search');
+let $btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById('btn');
 
-class QueryWorker { 
-	#search: HTMLInputElement;
-	#btn: HTMLButtonElement;
-
-	constructor() { 
-		this.#search = <HTMLInputElement>document.getElementById('search');
-		this.#btn = <HTMLInputElement>document.getElementById('btn');
-	}
-
-
-
+function searchText() {
+  return $search.value;
 }
+
+let findBook = async () => {
+  let bookName = searchText();
+  let books = await getBooks(bookName);
+
+  CardManager.clean();
+  CardManager.add(...books);
+};
+
+$btn.addEventListener('click', findBook);
